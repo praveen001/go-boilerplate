b1a043574ea6c8606ba58e2a1a2b0bc42297b65c
Praveen <geeks4share@gmail.com>
2019-02-22 20:41:11 +0530

    partial

--- handlers/user.go
+++ handlers/user.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi"
 	"github.com/praveen001/go-boilerplate/app"
@@ -29,40 +32,96 @@ func NewUserHandler(c *app.Context) *UserHandler {
 
 // Create .
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "New User")
+	var u *models.User
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		h.logger.Error("Unable to decode request body", err)
+		return
+	}
+
+	if err := h.user.New(u); err != nil {
+		h.logger.Error("Unable to create new user", err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(u)
+
 }
 
 // List .
 func (h *UserHandler) List(w http.ResponseWriter, r *http.Request) {
-	u, _ := h.user.FindAll()
-	h.logger.Info(u)
-	fmt.Fprintln(w, "List all users")
+	u, err := h.user.All()
+	if err != nil {
+		h.logger.Error("Unable to fetch users", err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(u)
 }
 
 // DeleteAll .
 func (h *UserHandler) DeleteAll(w http.ResponseWriter, r *http.Request) {
+	if err := h.user.DeleteAll(); err != nil {
+		h.logger.Error("Unable to delete users", err)
+		return
+	}
 	fmt.Fprintln(w, "Delete all users")
 }
 
 // Get ..
 func (h *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Get User By ID", chi.URLParam(r, "userID"))
+	u := r.Context().Value("user")
+
+	json.NewEncoder(w).Encode(u)
 }
 
 // Update ..
 func (h *UserHandler) Update(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Update User By ID", chi.URLParam(r, "userID"))
+	var u models.User
+
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		h.logger.Error("Unable to decode request body", err)
+		return
+	}
+
+	if err := h.user.Update(&u); err != nil {
+		h.logger.Error("Unable to update user", err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(u)
 }
 
 // Delete ..
 func (h *UserHandler) Delete(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Delete User By ID", chi.URLParam(r, "userID"))
+	u := r.Context().Value("user").(*models.User)
+
+	if err := h.user.Delete(u.ID); err != nil {
+		h.logger.Error("Unable to delete user", err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(u)
 }
 
 // Preload .
 func (h *UserHandler) Preload(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Loading users from middleware")
-		next.ServeHTTP(w, r)
+		rawUserID := chi.URLParam(r, "userID")
+
+		userID, err := strconv.Atoi(rawUserID)
+		if err != nil {
+			h.logger.Error("Bad Request", err)
+			return
+		}
+
+		u, err := h.user.Find(uint(userID))
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			h.logger.Error("Unable to find user", err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", u)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
--- models/user.go
+++ models/user.go
@@ -1,13 +1,18 @@
 package models
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 )
 
 // User ..
 type User struct {
-	gorm.Model
-	Name string `json:"name"`
+	ID        uint       `json:"id" gorm:"primary_key"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt time.Time  `json:"updatedAt"`
+	DeletedAt *time.Time `json:"-" sql:"index"`
+	Name      string     `json:"name"`
 }
 
 // UserService .
@@ -20,7 +25,34 @@ func NewUserService(c *gorm.DB) *UserService {
 	return &UserService{c}
 }
 
-// FindAll .
-func (u *UserService) FindAll() ([]*User, error) {
-	return nil, nil
+// New creates a new user
+func (s *UserService) New(user *User) error {
+	return s.db.Create(user).Error
+}
+
+// All .
+func (s *UserService) All() ([]*User, error) {
+	var u []*User
+	return u, s.db.Find(&u).Error
+}
+
+// DeleteAll .
+func (s *UserService) DeleteAll() error {
+	return s.db.Unscoped().Delete(User{}).Error
+}
+
+// Find .
+func (s *UserService) Find(ID uint) (*User, error) {
+	var u User
+	return &u, s.db.First(&u, ID).Error
+}
+
+// Delete .
+func (s *UserService) Delete(ID uint) error {
+	return s.db.Delete(&User{}, ID).Error
+}
+
+// Update ..
+func (s *UserService) Update(u *User) error {
+	return s.db.Model(&u).Update(u).Error
 }
